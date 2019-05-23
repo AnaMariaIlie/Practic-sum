42b11e67b71d72785878c1fa637402cfad62c589
Ilie Ana <ilieanamaria46@yahoo.com>
2019-05-23 11:56:42 +0300

    first commit

---
+++ app/src/main/java/ro/pub/cs/systems/eim/practicaltest02/network/ClientThread.java
@@ -0,0 +1,88 @@
+package ro.pub.cs.systems.eim.practicaltest02.network;
+
+import android.util.Log;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+import ro.pub.cs.systems.eim.practicaltest02.general.Constants;
+import ro.pub.cs.systems.eim.practicaltest02.general.Utilities;
+
+public class ClientThread extends Thread {
+
+    private String address;
+    private int port;
+    private String operand1;
+    private String operand2;
+    private String informationType;
+    private TextView weatherForecastTextView;
+
+    private Socket socket;
+
+    public ClientThread(String address, int port, String operand1, String operand2, String informationType, TextView weatherForecastTextView) {
+        this.address = address;
+        this.port = port;
+        this.operand1 = operand1;
+        this.operand2 = operand2;
+        this.informationType = informationType;
+        this.weatherForecastTextView = weatherForecastTextView;
+    }
+
+    @Override
+    public void run() {
+        try {
+
+            socket = new Socket(address, port);
+            if (socket == null) {
+                Log.e(Constants.TAG, "[CLIENT THREAD] Could not create socket!");
+                return;
+            }
+            BufferedReader bufferedReader = Utilities.getReader(socket);
+            PrintWriter printWriter = Utilities.getWriter(socket);
+            if (bufferedReader == null || printWriter == null) {
+                Log.e(Constants.TAG, "[CLIENT THREAD] Buffered Reader / Print Writer are null!");
+                return;
+            }
+            printWriter.println(operand1);
+            printWriter.flush();
+
+            printWriter.println(operand2);
+            printWriter.flush();
+
+
+            printWriter.println(informationType);
+            printWriter.flush();
+
+            String weatherInformation;
+            while ((weatherInformation = bufferedReader.readLine()) != null) {
+                final String finalizedWeateherInformation = weatherInformation;
+                weatherForecastTextView.post(new Runnable() {
+                   @Override
+                    public void run() {
+                       weatherForecastTextView.setText(finalizedWeateherInformation);
+                   }
+                });
+            }
+        } catch (IOException ioException) {
+            Log.e(Constants.TAG, "[CLIENT THREAD] An exception has occurred: " + ioException.getMessage());
+            if (Constants.DEBUG) {
+                ioException.printStackTrace();
+            }
+        } finally {
+            if (socket != null) {
+                try {
+                    socket.close();
+                } catch (IOException ioException) {
+                    Log.e(Constants.TAG, "[CLIENT THREAD] An exception has occurred: " + ioException.getMessage());
+                    if (Constants.DEBUG) {
+                        ioException.printStackTrace();
+                    }
+                }
+            }
+        }
+    }
+
+}
